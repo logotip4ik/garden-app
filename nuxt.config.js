7acd8b074d06addcccf33248c737663e21654faa
logotip4ik <bogdankostyuk12@gmail.com>
2021-07-06 13:18:12 +0300

    update: added descritpion to website

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Вебсайт-щоденник для ведення записів про ваш садок ',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
