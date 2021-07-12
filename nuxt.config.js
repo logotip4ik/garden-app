bc24533942e05082c58c88cc58abc7ed74a2c3a8
logotip4ik <bogdankostyuk12@gmail.com>
2021-07-12 22:49:04 +0300

    update: now it is in ssr and server mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-  // ssr: true,
+  target: 'server',
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
