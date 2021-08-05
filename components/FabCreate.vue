5ccee75e5c1b461e1f325d588b22b4aaaaea19b1
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-08-05 13:03:09 +0300

    update: replaced button with link in fab

--- components/FabCreate.vue
+++ components/FabCreate.vue
@@ -1,23 +1,13 @@
 <template>
   <transition :css="false" @enter="enterAnim" @leave="leaveAnim">
-    <button
+    <NuxtLink
       v-if="$route.name === 'year' || $route.name === 'year-group'"
+      prefetch
+      :to="createUrl"
       class="fab"
-      @click="
-        () =>
-          $router.push({
-            name: 'create-type',
-            params: {
-              type: $route.name === 'year' ? 'groups' : 'plants',
-            },
-            query: {
-              year: $route.params.year,
-            },
-          })
-      "
     >
-      <span class="material-icons .md-48">add</span>
-    </button>
+      <span class="material-icons">add</span>
+    </NuxtLink>
   </transition>
 </template>
 
@@ -25,6 +15,13 @@
 import gsap from 'gsap'
 
 export default {
+  computed: {
+    createUrl() {
+      const { year } = this.$route.params
+      const createType = this.$route.name === 'year' ? 'groups' : 'plants'
+      return `/create/${createType}?year=${year}`
+    },
+  },
   methods: {
     enterAnim(el, onComplete) {
       gsap.fromTo(
@@ -43,6 +40,9 @@ export default {
 <style lang="scss">
 .fab {
   $fab-size: 50px;
+  width: $fab-size;
+  height: $fab-size;
+
   position: fixed;
   bottom: 5%;
   right: 10%;
@@ -50,16 +50,21 @@ export default {
   right: clamp(1rem, 10%, 4rem);
   // bottom: clamp(2rem, 1vh, 4rem);
   // right: clamp(2rem, 10vw, 4rem);
-  border-radius: 50%;
+
+  color: white;
   box-shadow: 0 0 2px rgba($color: #000000, $alpha: 0.25),
     1px 5px 10px rgba($color: #000000, $alpha: 0.25);
   background-color: var(--secondary-color);
   border: none;
-  width: $fab-size;
-  height: $fab-size;
+  border-radius: 50%;
+  text-decoration: none;
+
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
   cursor: pointer;
   transition: background-color 200ms ease-out, color 200ms ease-out;
-  color: white;
 
   &:hover {
     background-color: var(--secondary-color-dark);
